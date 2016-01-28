076e605bdda90eb6283ffbc5f93552ddde702ba1
Mike Flynn <mfflynn@gmail.com>
2016-01-28 00:49:01 -0800

    Updated with better structure for events.

--- roommate-cli/events.go
+++ roommate-cli/events.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 
 	"github.com/deckarep/gosx-notifier"
 )
@@ -10,65 +17,295 @@ var EventList map[string]*Event = map[string]*Event{
 	"wallpaper": &Event{
 		Description:    "Changes the wallpaper on the desktop.",
 		ArgDescription: "<absolute path to image>",
-		Attributes: AttributeList{
-			Clean:  50,
-			Angry:  50,
-			Creepy: 50,
-		},
-		Options: {},
-		Fn: func(...string) string {
-			cmd := fmt.Sprintf("tell application \"Finder\" to set desktop picture to POSIX file \"%s\"", imgPath)
-			actionScript(cmd)
+		Fn: func(args ...string) string {
+			if len(args) == 0 {
+				return "Not enough arguments."
+			} else {
+				cmd := fmt.Sprintf("tell application \"Finder\" to set desktop picture to POSIX file \"%s\"", args[0])
+				actionScript(cmd)
+			}
+			return "Wallpaper set."
 		},
 	},
 	"notify": &Event{
-		ArgDescription: "",
+		Description:    "Fires a notification to the screen.",
+		ArgDescription: "<title> <body> <image>",
+		Fn: func(args ...string) string {
+			if len(args) < 4 {
+				return "Not enough arguments."
+			} else {
+				n := &Notification{
+					Body:  args[0],
+					Title: args[1],
+					Image: args[2],
+				}
+
+				err := n.notify()
+				if err != nil {
+					return err.Error()
+				}
+
+				return "Notification sent."
+			}
+		},
 	},
 	"quicklook": &Event{
+		Description:    "Opens quicklook with a file.",
 		ArgDescription: "<absolute path to image>",
+		Fn: func(args ...string) string {
+			if len(args) == 0 {
+				return "Not enough arguments."
+			} else {
+				cmd := exec.Command("qlmanage", "-p", args[0])
+				var out bytes.Buffer
+				cmd.Stdout = &out
+				err := cmd.Run()
+				if err != nil {
+					return err.Error()
+				}
+
+				list := out.String()
+				if list != "" {
+					return list
+				}
+
+				return "Quicklook popped."
+			}
+		},
 	},
 	"movefile": &Event{
+		Description:    "Moves the specified file to the specified place.",
 		ArgDescription: "<absolute path to source> <absolute path to desitination>",
+		Fn: func(args ...string) string {
+			if len(args) < 3 {
+				return "Not enough arguments."
+			} else {
+				if err := os.Rename(args[0], args[1]); err != nil {
+					return err.Error()
+				} else {
+					return "File moved."
+				}
+			}
+		},
 	},
 	"openfile": &Event{
+		Description:    "Opens the specified file.",
 		ArgDescription: "<absolute path to file>",
+		Fn: func(args ...string) string {
+			if len(args) == 0 {
+				return "Not enough arguments."
+			} else {
+				cmd := exec.Command("open", args[0])
+				var out bytes.Buffer
+				cmd.Stdout = &out
+				err := cmd.Run()
+				if err != nil {
+					return err.Error()
+				}
+
+				if list := out.String(); list != "" {
+					return list
+				}
+
+				return "File opened."
+			}
+		},
 	},
 	"makedir": &Event{
+		Description:    "Creates a directory.",
 		ArgDescription: "<absolute path to new directory>",
+		Fn: func(args ...string) string {
+			if len(args) == 0 {
+				return "Not enough arguments."
+			} else {
+				if err := os.MkdirAll(args[0], 0777); err != nil {
+					return err.Error()
+				}
+
+				return "Directory created."
+			}
+		},
 	},
 	"makefile": &Event{
+		Description:    "Generates a file with the required number of characters.",
 		ArgDescription: "<absolute path to new file> <number of chars to fill it with>",
+		Fn: func(args ...string) string {
+			var charCount uint64
+			var filePath string
+			var err error
+
+			if len(args) == 0 {
+				return "Not enough arguments."
+			} else if len(args) == 1 {
+				filePath = args[0]
+				charCount = 100
+			} else {
+				filePath = args[0]
+				if charCount, err = strconv.ParseUint(args[2], 10, 64); err != nil {
+					return err.Error()
+				}
+			}
+
+			var letters = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+			var f *os.File
+
+			b := make([]rune, charCount)
+			for i := range b {
+				b[i] = letters[rand.Intn(len(letters))]
+			}
+
+			if f, err = os.Create(filePath); err != nil {
+				return err.Error()
+			}
+
+			if _, err = f.WriteString(string(b)); err != nil {
+				return err.Error()
+			}
+
+			return "Directory created."
+		},
 	},
 	"openapp": &Event{
+		Description:    "Opens an app, could be in the background.",
 		ArgDescription: "<app name> <background flag>",
+		Fn: func(args ...string) string {
+			var inBackground bool
+
+			if len(args) > 1 && args[1] != "0" {
+				inBackground = true
+			} else if len(args) > 0 {
+				inBackground = false
+			} else {
+				return "Not enough arguments."
+			}
+
+			var cmd *exec.Cmd
+			if inBackground {
+				cmd = exec.Command("open", "-a", args[0], "-g")
+			} else {
+				cmd = exec.Command("open", "-a", args[0])
+			}
+
+			var out bytes.Buffer
+			cmd.Stdout = &out
+			err := cmd.Run()
+			if err != nil {
+				return err.Error()
+			}
+
+			if list := out.String(); err != nil {
+				return list
+			}
+
+			return "App opened."
+		},
 	},
 	"closeapp": &Event{
+		Description:    "Closes an app.",
 		ArgDescription: "<app name>",
+		Fn: func(args ...string) string {
+			if len(args) == 0 {
+				return "Not enough arguments."
+			} else {
+				closeApp(args[0])
+			}
+
+			return "App closed."
+		},
 	},
 	"brightness": &Event{
+		Description:    "Adjusts brightness level.",
 		ArgDescription: "<brightness level 0 - 1; ex: 0.3>",
+		Fn: func(args ...string) string {
+			if len(args) == 0 {
+				return "Not enough arguments."
+			} else {
+				if output, err := storedActionScript("brightness.applescript", args[0]); err != nil {
+					return err.Error()
+				} else {
+					return output
+				}
+			}
+		},
 	},
 	"alert": &Event{
+		Description:    "Generates OS X alert box.",
 		ArgDescription: "<body> <title> <icon path> <button_1_text> <button_2_text>",
+		Fn: func(args ...string) string {
+			if len(args) < 6 {
+				return "Not enough arguments."
+			} else {
+				if output, err := storedActionScript("alert.applescript", args[0], args[1], asPath(args[2]), args[3], args[4]); err != nil {
+					return err.Error()
+				} else {
+					return output
+				}
+			}
+		},
 	},
 	"volume": &Event{
+		Description:    "Adjusts volume without UI",
 		ArgDescription: "<volume % 0 - 100>",
+		Fn: func(args ...string) string {
+			if len(args) == 0 {
+				return "Not enough arguments."
+			} else {
+				if output, err := actionScript(fmt.Sprintf("set volume output volume %s --100%", args[0])); err != nil {
+					return err.Error()
+				} else {
+					return output
+				}
+			}
+		},
 	},
 	"startaudio": &Event{
+		Description:    "Starts playing audio file.",
 		ArgDescription: "<absolute path to audio>",
+		Fn: func(args ...string) string {
+			if len(args) == 0 {
+				return "Not enough arguments."
+			}
+
+			if *StartRepl {
+				go termCommand("afplay", args[0])
+			} else {
+				if out, err := termCommand("afplay", args[0]); err != nil {
+					return err.Error()
+				} else if out != "" {
+					return out
+				}
+			}
+
+			return "Audio started"
+		},
 	},
 	"stopaudio": &Event{
+		Description:    "Stops audio.",
 		ArgDescription: "",
+		Fn: func(args ...string) string {
+			var out string
+			var err error
+			if out, err = termCommand("killall", "afplay"); err != nil {
+				return err.Error()
+			}
+
+			return out
+		},
 	},
-	"commands": &Event{
-		ArgDescription: "",
-	},
+}
+
+func ShowCommands() string {
+	var output string
+	for cmd, info := range EventList {
+		output = output + fmt.Sprintf("%s %s -- %s\n", cmd, info.ArgDescription, info.Description)
+	}
+
+	return output
 }
 
 type Event struct {
 	Description     string                 // Name of event for logging.
 	ArgDescription  string                 // A description of what arguments it takes.
-	Attributes      AttributeList          // Descriptors
 	Options         map[string]string      // Various options.
 	Fn              func(...string) string // Run method
 	FollowedBy      []*Event               // Links to events that may come directly after this one.
@@ -153,7 +390,7 @@ func actionScript(command string) (string, error) {
 }
 
 func termCommand(args ...string) (string, error) {
-	cmd := exec.Command(args...)
+	cmd := exec.Command(args[0], args[1:]...)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
@@ -163,4 +400,16 @@ func termCommand(args ...string) (string, error) {
 
 	list := out.String()
 	return list, nil
+}
+
+func closeApp(appName string) {
+	actionScript(fmt.Sprintf("quit app \"%s\"", appName))
+}
+
+func asPath(path string) string {
+	if strings.HasPrefix(path, "/") {
+		path = "Macintosh HD" + path
+	}
+
+	return strings.Replace(path, "/", ":", -1)
 }
--- roommate-cli/main.go
+++ roommate-cli/main.go
@@ -2,34 +2,25 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	//"math"
-	"math/rand"
 	"os"
-	"os/exec"
-	"regexp"
-	"strconv"
 	"strings"
-	"time"
-
-	"github.com/deckarep/gosx-notifier"
 )
 
-var startRepl *bool
-var startService *bool
+var StartRepl *bool
+var StartService *bool
 
 func main() {
-	startRepl = flag.Bool("repl", false, "Start an interactive repl for command testing.")
-	startService = flag.Bool("service", false, "Start the app watching and random task service.")
+	StartRepl = flag.Bool("repl", false, "Start an interactive repl for command testing.")
+	StartService = flag.Bool("service", false, "Start the app watching and random task service.")
 	flag.Parse()
 
-	if *startRepl {
+	if *StartRepl {
 		repl()
-	} else if *startService {
+	} else if *StartService {
 		service()
 	} else if len(os.Args) > 1 {
 		args := os.Args[1:]
@@ -66,158 +57,19 @@ func repl() {
 }
 
 func parseCommand(args []string) {
-	var err error
-
-	switch {
-	case args[0] == "notify":
-		n := &Notification{
-			Body:     "Can you pick some up?",
-			Title:    "Hey...",
-			Subtitle: "We're out of your milk.",
-			Image:    "/Applications/ComputerRoommate.app/Contents/Resources/icon.ico",
-		}
-
-		err := n.notify()
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		fmt.Println("Notification sent.")
-	case args[0] == "wallpaper" && len(args) > 1:
-		changeWallpaper(args[1])
-
-		fmt.Println("Wallpaper updated.")
-	case args[0] == "openapp" && len(args) > 1:
-		if len(args) > 2 && args[2] != "0" {
-			openApp(args[1], true)
-		} else {
-			openApp(args[1], false)
-		}
-
-		fmt.Println("App opened.")
-	case args[0] == "closeapp" && len(args) > 1:
-		closeApp(args[1])
-
-		fmt.Println("App closed.")
-	case args[0] == "quicklook" && len(args) > 1:
-		_, err := quickLook(args[1])
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		fmt.Println("Quicklook popped.")
-	case args[0] == "startaudio" && len(args) > 1:
-		if *startRepl {
-			go func() {
-				if out, err := startAudio(args[1]); err != nil {
-					fmt.Println(err.Error())
-				} else {
-					fmt.Println(out)
-				}
-			}()
-		} else {
-			if out, err := startAudio(args[1]); err != nil {
-				fmt.Println(err.Error())
-			} else {
-				fmt.Println(out)
-			}
-		}
-
-		fmt.Println("Audio playing.")
-	case args[0] == "stopaudio":
-		_, err := stopAudio()
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		fmt.Println("Audio stopped.")
-	case args[0] == "openfile" && len(args) > 1:
-		_, err := openFile(args[1])
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		fmt.Println("That thing opened.")
-	case args[0] == "makedir" && len(args) > 1:
-		if err := createDir(args[1]); err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		fmt.Println("Directory created.")
-	case args[0] == "makefile" && len(args) > 1:
-		var count uint64 = 0
-		if len(args) > 2 {
-			if count, err = strconv.ParseUint(args[2], 10, 64); err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-		}
-		if err := createFile(args[1], count); err != nil {
-			fmt.Println(err.Error())
-		} else {
-			fmt.Println("Directory created.")
-		}
-	case args[0] == "movefile" && len(args) > 2:
-		if err := moveFile(args[1], args[2]); err != nil {
-			fmt.Println(err.Error())
-		}
-	case args[0] == "brightness" && len(args) > 1:
-		if output, err := storedActionScript("brightness.applescript", args[1]); err != nil {
-			fmt.Println(err.Error())
-		} else {
-			fmt.Println(output)
-		}
-	case args[0] == "alert" && len(args) > 5:
-		if output, err := storedActionScript("alert.applescript", args[1], args[2], asPath(args[3]), args[4], args[5]); err != nil {
-			fmt.Println(err.Error())
-		} else {
-			fmt.Println(output)
-		}
-	case args[0] == "volume" && len(args) > 1:
-		if _, err := actionScript(fmt.Sprintf("set volume output volume %s --100%", args[1])); err != nil {
-			fmt.Println(err.Error())
-		} else {
-			fmt.Println("Volume set to " + args[1])
-		}
-	case args[0] == "commands":
-		fmt.Println("Valid commands:\n" +
-			"notify\n" +
-			"wallpaper <absolute path to image>\n" +
-			"quicklook <absolute path to image>\n" +
-			"movefile <absolute path to source> <absolute path to desitination>\n" +
-			"openfile <absolute path to file>\n" +
-			"makedir <absolute path to new directory>\n" +
-			"makefile <absolute path to new file> <number of chars to fill it with>\n" +
-			"openapp <app name> <background flag>\n" +
-			"closeapp <app name>\n" +
-			"brightness <brightness level 0 - 1; ex: 0.3>\n" +
-			"alert <body> <title> <icon path> <button_1_text> <button_2_text>\n" +
-			"volume <volume % 0 - 100>\n" +
-			"startaudio <absolute path to audio>\n" +
-			"stopaudio\n")
-	case args[0] == "quit":
-		fmt.Println("Bye!")
+	if args[0] == "commands" {
+		fmt.Println(ShowCommands())
+	} else if args[0] == "quit" {
 		os.Exit(0)
-	default:
+	} else if event, ok := EventList[args[0]]; ok {
+		event.Run(args[1:]...)
+	} else {
 		fmt.Println("Invalid command. Try \"commands\" for a command list.")
 	}
 }
 
-func asPath(path string) string {
-	if strings.HasPrefix(path, "/") {
-		path = "Macintosh HD" + path
-	}
-
-	return strings.Replace(path, "/", ":", -1)
-}
-
 func watchPs() {
-	found := map[string]bool{}
+	//found := map[string]bool{}
 
 	/*
 		go func() {
@@ -238,256 +90,79 @@ func watchPs() {
 			}
 		}()
 	*/
+	/*
+		for {
+			cmd := exec.Command("ps", "-ax")
+			var out bytes.Buffer
+			cmd.Stdout = &out
+			err := cmd.Run()
+			if err != nil {
+				log.Fatal(err)
+			}
 
-	for {
-		cmd := exec.Command("ps", "-ax")
-		var out bytes.Buffer
-		cmd.Stdout = &out
-		err := cmd.Run()
-		if err != nil {
-			log.Fatal(err)
-		}
+			list := out.String()
+			if matched, err := regexp.MatchString("MacOS/iTunes\\b", list); err == nil && matched {
+				name := "iTunes"
+				if _, ok := found[name]; !ok {
+					found[name] = true
+					go func() {
+						time.Sleep(1000 * time.Millisecond * 1)
 
-		list := out.String()
-		if matched, err := regexp.MatchString("MacOS/iTunes\\b", list); err == nil && matched {
-			name := "iTunes"
-			if _, ok := found[name]; !ok {
-				found[name] = true
-				go func() {
-					time.Sleep(1000 * time.Millisecond * 1)
+						closeApp("iTunes")
 
-					closeApp("iTunes")
+						if output, err := storedActionScript("alert.applescript", "I'm in here!", "Ugggghhh!", asPath("/Applications/ComputerRoommate.app/Contents/Resources/icon.ico"), "Ok", "Hurry up!"); err != nil {
+							fmt.Println(err.Error())
+						} else {
+							fmt.Println(output)
+						}
 
-					if output, err := storedActionScript("alert.applescript", "I'm in here!", "Ugggghhh!", asPath("/Applications/ComputerRoommate.app/Contents/Resources/icon.ico"), "Ok", "Hurry up!"); err != nil {
-						fmt.Println(err.Error())
-					} else {
-						fmt.Println(output)
-					}
+						// Close it.
+						delete(found, name)
+					}()
+				}
+			}
 
-					// Close it.
-					delete(found, name)
-				}()
+			if matched, err := regexp.MatchString("MacOS/Safari\\b", list); err == nil && matched {
+				name := "Safari"
+				if _, ok := found[name]; !ok {
+					found[name] = true
+					go func() {
+						time.Sleep(1000 * time.Millisecond * 1)
+
+						closeApp("Safari")
+
+						if output, err := storedActionScript("alert.applescript", "I'll be out in a minute!", "Please Knock!", asPath("/Applications/ComputerRoommate.app/Contents/Resources/icon.ico"), "Ok", "Hurry up!"); err != nil {
+							fmt.Println(err.Error())
+						} else {
+							fmt.Println(output)
+						}
+
+						// Close it.
+						delete(found, name)
+					}()
+				}
+			}
+
+			if matched, err := regexp.MatchString("MacOS/Keynote\\b", list); err == nil && matched {
+				name := "Keynote"
+				if _, ok := found[name]; !ok {
+					found[name] = true
+					go func() {
+						time.Sleep(1000 * time.Millisecond * 1)
+
+						closeApp("Keynote")
+
+						if output, err := storedActionScript("alert.applescript", "Give me a second!", "Ugggghhh!", asPath("/Applications/ComputerRoommate.app/Contents/Resources/icon.ico"), "Ok", "Hurry up!"); err != nil {
+							fmt.Println(err.Error())
+						} else {
+							fmt.Println(output)
+						}
+
+						// Close it.
+						delete(found, name)
+					}()
+				}
 			}
 		}
-
-		if matched, err := regexp.MatchString("MacOS/Safari\\b", list); err == nil && matched {
-			name := "Safari"
-			if _, ok := found[name]; !ok {
-				found[name] = true
-				go func() {
-					time.Sleep(1000 * time.Millisecond * 1)
-
-					closeApp("Safari")
-
-					if output, err := storedActionScript("alert.applescript", "I'll be out in a minute!", "Please Knock!", asPath("/Applications/ComputerRoommate.app/Contents/Resources/icon.ico"), "Ok", "Hurry up!"); err != nil {
-						fmt.Println(err.Error())
-					} else {
-						fmt.Println(output)
-					}
-
-					// Close it.
-					delete(found, name)
-				}()
-			}
-		}
-
-		if matched, err := regexp.MatchString("MacOS/Keynote\\b", list); err == nil && matched {
-			name := "Keynote"
-			if _, ok := found[name]; !ok {
-				found[name] = true
-				go func() {
-					time.Sleep(1000 * time.Millisecond * 1)
-
-					closeApp("Keynote")
-
-					if output, err := storedActionScript("alert.applescript", "Give me a second!", "Ugggghhh!", asPath("/Applications/ComputerRoommate.app/Contents/Resources/icon.ico"), "Ok", "Hurry up!"); err != nil {
-						fmt.Println(err.Error())
-					} else {
-						fmt.Println(output)
-					}
-
-					// Close it.
-					delete(found, name)
-				}()
-			}
-		}
-	}
-}
-
-type Notification struct {
-	Body     string
-	Title    string
-	Subtitle string
-	Image    string
-	Icon     string
-}
-
-func (this *Notification) notify() error {
-	note := gosxnotifier.NewNotification(this.Body)
-	note.Title = this.Title
-	note.Subtitle = this.Subtitle
-	note.Group = "com.roommate.cli.chd10"
-
-	if this.Icon != "" {
-		note.AppIcon = this.Icon
-	}
-
-	if this.Image != "" {
-		note.ContentImage = this.Image
-	}
-
-	note.Sound = gosxnotifier.Basso
-
-	return note.Push()
-}
-
-func createFile(filePath string, charCount uint64) error {
-	var letters = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	var f *os.File
-	var err error
-
-	b := make([]rune, charCount)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-
-	if f, err = os.Create(filePath); err != nil {
-		return err
-	}
-
-	if _, err = f.WriteString(string(b)); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func createDir(dirPath string) error {
-	return os.MkdirAll(dirPath, 0777)
-}
-
-func stopAudio() (string, error) {
-	cmd := exec.Command("killall", "afplay")
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		return "", err
-	}
-
-	list := out.String()
-	return list, nil
-}
-
-func startAudio(filePath string) (string, error) {
-	cmd := exec.Command("afplay", filePath)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		return "", err
-	}
-
-	list := out.String()
-	return list, nil
-}
-
-func openFile(filePath string) (string, error) {
-	cmd := exec.Command("open", filePath)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		return "", err
-	}
-
-	list := out.String()
-	return list, nil
-}
-
-func openApp(appName string, inBackground bool) (string, error) {
-	var cmd *exec.Cmd
-	if inBackground {
-		cmd = exec.Command("open", "-a", appName, "-g")
-	} else {
-		cmd = exec.Command("open", "-a", appName)
-	}
-
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		return "", err
-	}
-
-	list := out.String()
-	return list, nil
-}
-
-func quickLook(imgPath string) (string, error) {
-	cmd := exec.Command("qlmanage", "-p", imgPath)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		return "", err
-	}
-
-	list := out.String()
-	return list, nil
-}
-
-func changeWallpaper(imgPath string) {
-	cmd := fmt.Sprintf("tell application \"Finder\" to set desktop picture to POSIX file \"%s\"", imgPath)
-	actionScript(cmd)
-}
-
-func closeApp(appName string) {
-	actionScript(fmt.Sprintf("quit app \"%s\"", appName))
-}
-
-func moveFile(sourcePath string, destPath string) error {
-	return os.Rename(sourcePath, destPath)
-}
-
-func storedActionScript(scriptName string, params ...string) (string, error) {
-	var data []byte
-	var err error
-
-	// Pull from asset store
-	if data, err = Asset("scripts/" + scriptName); err != nil {
-		return "", err
-	}
-
-	if err = ioutil.WriteFile("/tmp/"+scriptName, []byte(data), 0644); err != nil {
-		return "", err
-	}
-
-	if err = os.Chmod("/tmp/"+scriptName, 0777); err != nil {
-		return "", err
-	}
-
-	cmd := exec.Command("/tmp/"+scriptName, params...)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err = cmd.Run()
-	if err != nil {
-		return "", err
-	}
-
-	list := out.String()
-	return list, nil
-}
-
-func actionScript(command string) (string, error) {
-	cmd := exec.Command("osascript", "-e", command)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		return "", err
-	}
-
-	list := out.String()
-	return list, nil
+	*/
 }
